01838bb8104407522163ecadd6efac270bde5002
Jovennan <jovennan@gmail.com>
2018-03-10 20:20:00 -0300

    Tela Principal criada

---
+++ app/src/main/java/android_imc/jovennan/com/br/androidimc/MainActivity.java
@@ -0,0 +1,13 @@
+package android_imc.jovennan.com.br.androidimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
